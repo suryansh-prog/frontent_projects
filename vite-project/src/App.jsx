4ec74fb4e525927d43dedfa48c04355c081ed5df
suryansh soni <suryanshssoni232@gmail.com>
2025-10-28 23:41:54 +0530

    Fix App: move handlers, remove stray script; fix CSS button selector

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,23 +6,26 @@ const App = () => {
   const [count, setCount] = useState(0);
   const [increm, setIncrem] = useState(1);
   const [decre, setDecre] = useState(-1);
+  const handleIncrement = () => setCount(c => c + increm);
+
+  const handleDecrement = () => {
+    setCount(c => {
+      const next = c + decre;
+      return next < 0 ? 0 : next;
+    });
+  };
+
   return (
     <div className='App'>
       <h1 className='cnt'>Counter: {count}</h1>
 
       <div className='box'>
-        <button className='incre' onClick={() => setCount(count + increm)}>Increment</button>
-        <button className='decre' onClick={() => {
-          var res = count + decre;
-          if (res < 0) setCount(0);
-          else
-            setCount(res)
-        }}>Decrement</button>
+        <button className='incre' onClick={handleIncrement}>Increment</button>
+        <button className='decre' onClick={handleDecrement}>Decrement</button>
         <button onClick={() => setCount(0)}>Reset</button>
       </div>
     </div>
   )
 }
-<script></script>
 
 export default App
